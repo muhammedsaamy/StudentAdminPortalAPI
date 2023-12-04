445c56d8bb5353caab1bcee1c99017f4e91bd65c
muhammedsaamy <muhammedsaamy@gmail.com>
2023-12-04 22:08:29 +0200

    GetAllStudentsAsync added

--- StudentAdminPortalAPI/Controllers/StudentsController.cs
+++ StudentAdminPortalAPI/Controllers/StudentsController.cs
@@ -1,6 +1,32 @@
-﻿namespace StudentAdminPortalAPI.Controllers
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using StudentAdminPortalAPI.DomainModels;
+using StudentAdminPortalAPI.Repositories.Interfaces;
+
+namespace StudentAdminPortalAPI.Controllers
 {
-    public class StudentsController
+    public class StudentsController : BaseApiController
     {
+        private readonly IStudentRepository ManageStudents;
+        private readonly IMapper _mapper;
+
+        public StudentsController(IStudentRepository manageStudents, IMapper mapper)
+        {
+            ManageStudents = manageStudents;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        [Route("[controller]")]
+        public async Task<IActionResult> GetAllStudentsAsync()
+        {
+            var students = await ManageStudents.GetAllStudentsAsync();
+
+            return Ok(_mapper.Map<List<StudentDTO>>(students));
+        }
+
+
+
+
     }
 }
