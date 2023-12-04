66d4be25ec2db06e32849f4f994eb71e2cb0d216
muhammedsaamy <muhammedsaamy@gmail.com>
2023-12-04 19:32:02 +0200

    student database added

---
+++ StudentAdminPortalAPI/Models/Address.cs
@@ -0,0 +1,13 @@
+﻿namespace StudentAdminPortalAPI.Models
+{
+    public class Address
+    {
+        public Guid Id { get; set; }
+        public string? PhysicalAddress { get; set; }
+        public string? PostalAddress { get; set; }
+
+        //Navigation Properties
+        public Guid StudentId { get; set; }
+
+    }
+}
